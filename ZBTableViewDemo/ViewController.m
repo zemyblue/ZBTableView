f7dda1380a829d82c8d593804342a9bac178fbf3
Hongsup.So <zemyblue@gmail.com>
2014-11-04 23:43:47 +0900

    add Demo.

--- ZBTableViewDemo/ViewController.m
+++ ZBTableViewDemo/ViewController.m
@@ -7,21 +7,90 @@
 //
 
 #import "ViewController.h"
+#import "ZBTableView.h"
 
-@interface ViewController ()
-
+@interface ViewController () <ZBTableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) ZBTableView *tableView;
+@property (nonatomic, assign) NSUInteger numberOfRow;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.numberOfRow = 20;
+    
+    [self setTitle:@"ZBTableView Demo"];
+    
+    self.tableView = [[ZBTableView alloc] initWithFrame:self.view.bounds];
+    [self.tableView setDelegate:self];
+    [self.tableView setDataSource:self];
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark UITableViewDataSource
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.numberOfRow;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"cell %ld", indexPath.row + 1];
+    
+    return cell;
+}
+
+
+#pragma mark UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark ZBTableViewDataSource
+
+
+- (void)fetchRecentWithCompletion:(void (^)(NSError *aError))aCompletion
+{
+    // delay for test
+    __weak typeof(self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        weakSelf.numberOfRow = 20;
+        [weakSelf.tableView reloadData];
+        aCompletion(nil);
+    });
+}
+
+
+- (void)fetchMoreWithCompletion:(void (^)(NSError *aError))aCompletion
+{
+    // delay for test
+    __weak typeof(self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        weakSelf.numberOfRow += 20;
+        [weakSelf.tableView reloadData];
+        aCompletion(nil);
+    });
+}
+
 
 @end
